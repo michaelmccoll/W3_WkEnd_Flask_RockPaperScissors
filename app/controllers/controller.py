cdfd02394d6eee3bae87587ce13431ca032586b6
michaelmccoll <mccollmichael@hotmail.com>
2021-03-12 15:04:41 +0000

    Working for one result in HTML

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,9 +2,15 @@ from flask import render_template, request, redirect
 from app import app
 from app.model.player import Player
 
-# @app.route('/events')
+@app.route('/<player1>/<player2>')
+def index(player1,player2):
+    return render_template('index.html', title='Welcome Page', player1=player1, player2=player2)
+
+# @app.route('/<player1>/<player2>')
 # def index():
-#     return render_template('index.html', title='Events', events=events_list)
+#     player1_choice = player1
+#     player2_choice = player2
+#     return render_template('index.html', title='Welcome Page', player1=player1, player2=player2)
 
 # @app.route('/events', methods=['POST'])
 # def create_event():
