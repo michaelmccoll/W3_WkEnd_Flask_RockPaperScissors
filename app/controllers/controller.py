c7b802cf08cca13d7a05a5d1b62123efa56565c8
michaelmccoll <mccollmichael@hotmail.com>
2021-03-14 17:10:22 +0000

    Advanced extention working

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from app import app
-from app.model.game import game_result
+from app.model.game import Game, game_result
 from app.model.player import Player
 
 @app.route('/')
@@ -13,4 +13,11 @@ def show_game_result(player1,player2):
     player2_choice = player2
     outcome = game_result(player1_choice,player2_choice)
     return render_template('game_result.html', title='Game Result', outcome = outcome)
-    
+
+@app.route('/play', methods=['GET'])
+def play_pc():
+    name_of_player = request.form['player_name']
+    choice_of_player = request.form['player_choice']
+    play_pc = Game(name_of_player,choice_of_player)
+    outcome_vs_pc = game_result_vs_pc(play_pc) 
+    return render_template('play.html')
--- app/model/game.py
+++ app/model/game.py
@@ -1,18 +1,50 @@
 from app.model.player import *
+from random import choice
+
+class Game():
+    def __init__(self, name_of_player,choice_of_player):
+        self.name_of_player = name_of_player
+        self.choice_of_player = choice_of_player
 
 def game_result(player1_choice,player2_choice):
-    if player1_choice == player2_choice:
+    if player1_choice == "rock" and player2_choice =="rock":
+        return f"Its a DRAW!!!!"
+    if player1_choice == "paper" and player2_choice =="paper":
+        return f"Its a DRAW!!!!"
+    if player1_choice == "scissors" and player2_choice =="scissors":
         return f"Its a DRAW!!!!"
     elif player1_choice == "rock" and player2_choice == "scissors":
         return f"Player 1 WINS, {player1_choice} beats {player2_choice}"
     elif player1_choice == "rock" and player2_choice == "paper":
         return f"Player 2 WINS, {player2_choice} beats {player1_choice}"
     elif player1_choice == "paper" and player2_choice == "rock":
-        return f"Player 1 WINS, {player1_choice} beats {player1_choice}"
+        return f"Player 1 WINS, {player1_choice} beats {player2_choice}"
     elif player1_choice == "paper" and player2_choice == "scissors":
         return f"Player 2 WINS, {player2_choice} beats {player1_choice}"
     elif player1_choice == "scissors" and player2_choice == "paper":
-        return f"Player 1 WINS, {player1_choice} beats {player1_choice}"
+        return f"Player 1 WINS, {player1_choice} beats {player2_choice}"
     elif player1_choice == "scissors" and player2_choice == "rock":
         return f"Player 2 WINS, {player2_choice} beats {player1_choice}"
+    else: return f"Please type only type rock, paper or scissors"
+
+def game_result_vs_pc(name_of_player,choice_of_player):
+    pc_choice = choice(["rock","paper","scissors"])
+    if choice_of_player == "rock" and pc_choice =="rock":
+        return f"Its a DRAW!!!!"
+    if choice_of_player == "paper" and pc_choice =="paper":
+        return f"Its a DRAW!!!!"
+    if choice_of_player == "scissors" and pc_choice =="scissors":
+        return f"Its a DRAW!!!!"
+    elif choice_of_player == "rock" and pc_choice == "scissors":
+        return f"Player 1 WINS, {choice_of_player} beats {pc_choice}"
+    elif choice_of_player == "rock" and pc_choice == "paper":
+        return f"Player 2 WINS, {pc_choice} beats {choice_of_player}"
+    elif choice_of_player == "paper" and pc_choice == "rock":
+        return f"Player 1 WINS, {choice_of_player} beats {pc_choice}"
+    elif choice_of_player == "paper" and pc_choice == "scissors":
+        return f"Player 2 WINS, {pc_choice} beats {choice_of_player}"
+    elif choice_of_player == "scissors" and pc_choice == "paper":
+        return f"Player 1 WINS, {choice_of_player} beats {pc_choice}"
+    elif choice_of_player == "scissors" and pc_choice == "rock":
+        return f"Player 2 WINS, {pc_choice} beats {choice_of_player}"
     else: return f"Please type only type rock, paper or scissors"
